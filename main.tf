1853695c2e4f325b25f9e71914621b94119a7327
Yaroslav <ya.yaroslav27@gmail.com>
2024-10-01 16:59:09 +0200

    chore: ran 'terraform fmt' and fixed formatting issues

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ module "resource_group_storage" {
   source  = "yaroslavyvk/resource_group_storage/azurerm"
   version = "v1.1.0"
 
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  storage_account_name     = var.storage_account_name
+  resource_group_name  = var.resource_group_name
+  location             = var.location
+  storage_account_name = var.storage_account_name
 }
 
 output "resource_group_id" {
